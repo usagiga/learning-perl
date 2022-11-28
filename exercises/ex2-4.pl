8186006a7f072ca1aaec88d516d34271269e7603
usagiga <usagiga@usagiga.me>
2022-11-29 05:30:18 +0900

    feat: solve 2-4

---
+++ exercises/ex2-4.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use v5.24;
+use utf8;
+use strict;
+use warnings;
+use diagnostics;
+
+sub read_line {
+    my ( $prompt ) = @_;
+    print "${prompt}: ";
+
+    my $buf = <STDIN>;
+    chomp($buf);
+
+    return $buf;
+}
+
+my $a = read_line('a');
+my $b = read_line('b');
+
+my $prod = $a * $b;
+
+say $prod;
